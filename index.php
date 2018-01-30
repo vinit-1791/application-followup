0bb578f0cd3c76299f633b3247c2ceec34c905f3
vinit-1791 <vinit_1791@yahoo.co.in>
2018-01-29 23:40:33 -0600

    removed errors and added comments

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-
 class Interview
-{
-	public $title = 'Interview test';
+{   
+    // -----------------------------------------------------------------------------------
+    // Comment 1 :
+    // Declaring property title as static so that it can be accessed as Interview::$title
+    // -----------------------------------------------------------------------------------
+	static $title = 'Interview test';
 }
 
 $lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
@@ -15,7 +18,16 @@ $people = array(
 	array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
 );
 
-$person = $_POST['person'];
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+// Comment 2 :
+// Added a isset() condition to check if $_POST contains person index, to avoid undefined index error
+// Also initializing $person to null, in case the if loop is not executed, it will throw an error 'undefined variable' on line 80 & 81
+// -------------------------------------------------------------------------------------------------------------------------------------
+$person = null;
+if(isset($_POST['person'])) {
+    $person = $_POST['person'];
+}
 
 ?>
 
@@ -29,35 +41,43 @@ $person = $_POST['person'];
 	</style>
 </head>
 <body>
-
-	<h1><?=Interview::$title;?></h1>
-
+    <!--------------------------------------------------------------------------------------------------------------------------------------
+    // Comment 3 :
+    // title property of Interview class was being called statically. To do so, property needs to be defined as static. As done in comment 1
+    ---------------------------------------------------------------------------------------------------------------------------------------->
+    <h1><?=Interview::$title;?></h1>
+    
 	<?php
 	// Print 10 times
-	for ($i=10; $i<0; $i++) {
-		echo '<p>'+$lipsum+'</p>';
+    // ---------------------------------------------------------------------------------------------------------------------
+    // Comment 4 :
+    // If we initialize i at 10, our condition will never be true and for loop will not be executed
+    // Changed the initialization of i to 0 and and changed condition of for loop to i less than 10 to print string 10 times
+    // ---------------------------------------------------------------------------------------------------------------------
+	for ($i=0; $i<10; $i++) {
+        // --------------------------------------------------------
+        // comment 5 :
+        // used '.' (concatenation operator in PHP ) instead of '+'
+        // --------------------------------------------------------
+		echo '<p>'.$lipsum.'</p>';
 	}
 	?>
-
-
 	<hr>
-
-
-	<form method="get" action="<?=$_SERVER['REQUEST_URI'];?>">
+    <!-----------------------------------------------------------------------------------------------
+    Comment 6 :
+    Changed method of form to POST from GET
+    We can use get as well, but in that case, we have to change $_POST to $_GET on line no 28 & 29
+    ------------------------------------------------------------------------------------------------>
+	<form method="POST" action="<?=$_SERVER['REQUEST_URI'];?>">
 		<p><label for="firstName">First name</label> <input type="text" name="person[first_name]" id="firstName"></p>
 		<p><label for="lastName">Last name</label> <input type="text" name="person[last_name]" id="lastName"></p>
 		<p><label for="email">Email</label> <input type="text" name="person[email]" id="email"></p>
 		<p><input type="submit" value="Submit" /></p>
 	</form>
-
 	<?php if ($person): ?>
 		<p><strong>Person</strong> <?=$person['first_name'];?>, <?=$person['last_name'];?>, <?=$person['email'];?></p>
 	<?php endif; ?>
-
-
 	<hr>
-
-
 	<table>
 		<thead>
 			<tr>
@@ -69,13 +89,17 @@ $person = $_POST['person'];
 		<tbody>
 			<?php foreach ($people as $person): ?>
 				<tr>
-					<td><?=$person->first_name;?></td>
-					<td><?=$person->last_name;?></td>
-					<td><?=$person->email;?></td>
+                    <!-----------------------------------------------------------------------------------------------
+                    Comment 7 :
+                    Since $person is an array and not an object, we have to access its indexes and not the properties
+                    Hence changed $person->first_name to $person['first_name'] and likewise in below lines
+                    ------------------------------------------------------------------------------------------------>
+					<td><?=$person['first_name'];?></td>
+					<td><?=$person['last_name'];?></td>
+					<td><?=$person['email'];?></td>
 				</tr>
 			<?php endforeach; ?>
 		</tbody>
 	</table>
-
 </body>
 </html>
